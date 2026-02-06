82078314e60b7255ca8e773476405560cca21891
RogueL90 <pranayj.493@gmail.com>
2026-02-05 17:44:14 -0800

    Connects dist file to backend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 const Person = require('./model/person')
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 morgan.token('body', function (req) {
   return JSON.stringify(req.body)
