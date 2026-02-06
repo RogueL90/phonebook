97b4b5c92f74437011515b643fb1aba02b2a7666
RogueL90 <pranayj.493@gmail.com>
2026-02-05 17:11:57 -0800

    Added database and request constraints

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./model/person')
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 morgan.token('body', function (req) {
   return JSON.stringify(req.body)
 })
@@ -16,8 +17,9 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// No longer needed after implementation of mongoDB which auto generates an ID for each document
 
-
+/*
 let persons = [
     { 
       "id": "1",
@@ -42,53 +44,94 @@ let persons = [
 ]
 
 const generateId = () => String(Math.random());
+*/
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+  Person.find({}).then(res =>{
+    response.json(res)
+  })
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     const date = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date.toDateString()} ${date.toLocaleTimeString()}</p>`)
+    Person.find({})
+    .then(people => {
+      response.send(`<p>Phonebook has info for ${people.length} people</p><p>${date.toDateString()} ${date.toLocaleTimeString()}</p>`)
+    }).catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const person = persons.filter(person => person.id===id);
-    response.json(person);
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if(!person){
+        response.status(404).end()
+      }
+      response.json(person)
+    }).catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(person => person.id !== id);
-    response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(res => {
+      console.log(res)
+      response.status(204).end();
+    }).catch(err => next(err))
 })
 
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if(!person){
+      return response.status(404).end()
+    }
+    person.name = request.body.name
+    return person.save()
+    .then(updatedPerson => response.json(updatedPerson))
+  })
+  .catch(err => next(err))
+})
+
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
-    const newPerson = {
+    const newPerson = Person({
         name: body.name, 
         number: body.number,
-        id: generateId()
-    };
+    })
     if(!body.name || !body.number){
         console.log('name or number missing');
         return response.status(400).json({ 
     error: 'content missing' 
   })
     }
-    const duplicateCheck = persons.filter(person => person.name ===body.name);
-    if(duplicateCheck.length!=0){
-        console.log('no duplicate name');
+    Person.find({name: body.name}).then(after => {
+      if(after.length!=0){
+        console.log(after);
         return response.status(400).json({ 
     error: 'no dup names' 
   })
     }
-    persons = persons.concat(newPerson);
-    response.status(201).json(newPerson);
+    return newPerson.save().then(res => {
+      response.status(201).json(res);
+    })
+    })
+    .catch(err => next(err))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3001;
 app.listen(PORT, () => {
